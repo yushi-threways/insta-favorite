307bb49993c5f4882a2a7ac1c32d422ffec0899d
yushi-threways <seki@threways.co.jp>
2020-08-26 00:59:27 +0900

    インスタ自動いいね完成

--- oauth.py
+++ oauth.py
@@ -2,6 +2,7 @@ from selenium import webdriver #Selenium Webdriverをインポートして
 from selenium.webdriver.common.keys import Keys
 import urllib.parse
 import time
+import random
 
 browser = webdriver.Chrome("/usr/local/bin/chromedriver") #Chromeを動かすドライバを読み込み
 
@@ -9,7 +10,7 @@ browser = webdriver.Chrome("/usr/local/bin/chromedriver") #Chromeを動かすド
 loginURL = "https://www.instagram.com/" #ログインする際のページ
 tagSearchURL = "https://www.instagram.com/explore/tags/{}/?hl=ja" #.format()で{}の中の値を入れられるようになっている
 
-tagName = "ドールメイク" #タグの名前 #よみうりランド
+tagName = "ワンピースコーデ" #タグの名前 #よみうりランド
 
 
 #selectors
@@ -21,14 +22,14 @@ passwordPath = '//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/for
 
 notNowPath = '//*[@id="react-root"]/div/div[2]/a[2]'
 
-mediaSelector = 'div._9AhH0' #表示されているメディアのwebelement @https://www.instagram.com/explore/tags/%E3%82%88%E3%81%BF%E3%81%86%E3%82%8A%E3%83%A9%E3%83%B3%E3%83%89/?hl=ja
+mediaSelector = 'div.v1Nh3' #表示されているメディアのwebelement @https://www.instagram.com/explore/tags/%E3%82%88%E3%81%BF%E3%81%86%E3%82%8A%E3%83%A9%E3%83%B3%E3%83%89/?hl=ja
 likeXpath = '/html/body/div[3]/div/div[2]/div/article/div[2]/section[1]/a[1]'
 nextPagerSelector = 'a.coreSpriteRightPaginationArrow' #次へボタン
 
 #USER INFO
 
-username = "ronron.lolita"
-password = "ronron6646" 
+username = "lesmul.online"
+password = "lesmul5457" 
 
 #list
 
@@ -51,42 +52,40 @@ if __name__ == '__main__':
     passwordField.send_keys(password)
     passwordField.send_keys(Keys.RETURN)
 
-    time.sleep(3)
-    button = browser.find_element_by_css_selector(".cmbtv div > button")
-    button.click
-
-    print(button)
     #Finished logging in. now at 
     time.sleep(3)
-    # encodedTag = urllib.parse.quote(tagName) #普通にURLに日本語は入れられないので、エンコードする
-    # encodedURL = tagSearchURL.format(encodedTag)
-    # print("encodedURL:{}".format(encodedURL))
-    # browser.get(encodedURL)
+    encodedTag = urllib.parse.quote(tagName) #普通にURLに日本語は入れられないので、エンコードする
+    encodedURL = tagSearchURL.format(encodedTag)
+    print("encodedURL:{}".format(encodedURL))
+    browser.get(encodedURL)
 
     #Finished tag search. now at https://www.instagram.com/explore/tags/%E8%AA%AD%E5%A3%B2%E3%83%A9%E3%83%B3%E3%83%89/?hl=ja
-    # time.sleep(3)
-    # browser.implicitly_wait(10)
+    time.sleep(3)
+    browser.implicitly_wait(10)
 
     #写真を取得してクリックする
 
-    # mediaList = browser.find_elements_by_css_selector(mediaSelector)
-    # mediaCounter = len(mediaList)
-    # print("Found {} media".format(mediaCounter))
+    mediaList = browser.find_elements_by_css_selector(mediaSelector)
+    mediaCounter = len(mediaList)
+    print("Found {} media".format(mediaCounter))
 
-    # for media in mediaList:
-    #     media.click()
+    for media in mediaList:
+        media.click()
 
-    #     # 次へボタンが表示されるまで
-    #     while True:
-    #         try:
-    #             time.sleep(3)
-    #             browser.find_element_by_xpath(likeXpath).click()
-    #             browser.implicitly_wait(10)
-    #             likedCounter += 1
-    #             print("liked {} of {}".format(likedCounter,mediaCounter))
-    #             browser.find_element_by_css_selector(nextPagerSelector).click()
-    #         except:
-    #             break #もう次へボタンが存在しない場合、エラーをはくのでそこで終了
-    #     break #for文自体も終了させる
+        # 次へボタンが表示されるまで
+        while True:
+            try:
+                # abc = random.randint(random.randint(20, 100), random.randint(110, 180))
+                # print(str(abc)+"秒待機します")
+                time.sleep(6)
+                browser.find_element_by_class_name("fr66n").click()
+                browser.implicitly_wait(10)
+                likedCounter += 1
+                print("liked {} of {}".format(likedCounter,mediaCounter))
+                browser.find_element_by_css_selector(nextPagerSelector).click()
+            except Exception as e:
+                print(e)
+                browser.find_element_by_css_selector(nextPagerSelector).click()
+        break #for文自体も終了させる
 
     print("You liked {} media".format(likedCounter))
